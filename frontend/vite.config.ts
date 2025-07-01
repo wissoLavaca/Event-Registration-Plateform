8b7a871e6a1c1b1725bd2a05a8e3d2c2f9a11ef6
LaVaca <wissemrecham@gmail.com>
2025-07-01 18:33:23 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API requests
+      // API requests
       '/api': {
-        target: 'http://localhost:3001', // <<<< REPLACE WITH YOUR ACTUAL BACKEND SERVER ADDRESS AND PORT
-        changeOrigin: true, // Recommended for most cases
-        // secure: false,      // Uncomment if your backend is not HTTPS and you encounter issues
-        // rewrite: (path) => path.replace(/^\/api/, '') // Optional: if your backend routes don't expect the /api prefix
+        target: 'http://localhost:3001', 
+        changeOrigin: true, 
       }
     }
   },
